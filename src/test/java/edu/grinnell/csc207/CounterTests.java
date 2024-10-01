eb21a1e489422ba1068de0a0594ecb798e937588
sarah_deschamps <deschamps.sias@gmail.com>
2024-10-01 09:22:32 -0500

    Add NamedCounter class

--- src/test/java/edu/grinnell/csc207/CounterTests.java
+++ src/test/java/edu/grinnell/csc207/CounterTests.java
@@ -4,6 +4,7 @@ import edu.grinnell.csc207.util.BasicCounter;
 import edu.grinnell.csc207.util.Counter;
 import edu.grinnell.csc207.util.DecrementableCounter;
 import edu.grinnell.csc207.util.Tally;
+import edu.grinnell.csc207.util.NamedCounter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -23,7 +24,7 @@ public class CounterTests {
    */
   @Test
   public void test1() throws Exception {
-    Counter alpha = new Tally();
+    Counter alpha = new NamedCounter("alfa", 0);
     Counter beta = new BasicCounter(123);
     DecrementableCounter gamma = new DecrementableCounter(-5);
     assertEquals(0, alpha.get(), "original alpha");
