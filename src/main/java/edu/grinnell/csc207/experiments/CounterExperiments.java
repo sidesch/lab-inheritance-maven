fb12a977841e1eec6497a0befb6fe31e3253e28f
sarah_deschamps <deschamps.sias@gmail.com>
2024-10-01 09:47:41 -0500

    Add DblCtr.

--- src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -1,6 +1,7 @@
 package edu.grinnell.csc207.experiments;
 
 import edu.grinnell.csc207.util.Counter;
+import edu.grinnell.csc207.util.DblCtr;
 import edu.grinnell.csc207.util.DecrementableCounter;
 import edu.grinnell.csc207.util.DoubleCounter;
 import edu.grinnell.csc207.util.NamedCounter;
@@ -25,7 +26,7 @@ public class CounterExperiments {
 
     // Set up some counters
     Counter alpha = new NamedCounter("alfa", 0);
-    Counter beta = new DoubleCounter(123);
+    Counter beta = new DblCtr(new NamedCounter("dc", 0));
     BasicCounter gamma = new BoundedCounter(-5, 3);
 
     // Print original values
